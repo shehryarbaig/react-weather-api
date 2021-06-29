8ed583c2895d4ca2e1a66a3ab69063f8bdcb4fae
shehryar.baig <shehryar.baig@arbisoft.com>
2021-06-29 12:24:09 +0500

    adding method for getting day,updating logic of wc

--- src/App.js
+++ src/App.js
@@ -2,18 +2,34 @@ import './App.css';
 import SearchBar from "./components/SearchBar"
 import WeatherCard from "./components/WeatherCard"
 import {useState} from "react"
+import {APP_ID} from "./config";
 
 function App() {
-  const [query,setQuery] = useState("");
+  const [weatherData, setWeatherData] = useState();  
+
+  const fetchWeatherData = async (searchQuery) =>{
+    //const resp = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchQuery}&units=metric&appid=${APP_ID}`);
+    //console.log("inside fetch");
+    const resp = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${searchQuery}&units=metric&appid=${APP_ID}`);
+    const responseJson = await resp.json();
+    console.log(responseJson);
+    setWeatherData(responseJson);
+    
+  }
 
   const  handleCLick = (searchQuery)=>{
-    setQuery(searchQuery);
+    fetchWeatherData(searchQuery);
   };
 
   return (
     <div className="App">
-      <SearchBar onClick={handleCLick} placeholder="Enter City Name"/>
-      {query && <WeatherCard cityName = {query}/>}
+      <div className="searchSection">
+        <SearchBar onClick={handleCLick} placeholder="Enter City Name"/>
+      </div>
+      <div className="weatherCardSection">
+        {weatherData && <WeatherCard weatherData = {weatherData}/>}
+
+      </div>
     </div>
   );
 }
--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -1,25 +1,54 @@
 import {useEffect, useState} from 'react';
-import {APP_ID} from "../../config"
+import {APP_ID} from "../../config";
+import "./WeatherCard.style.css";
+import {getDayName} from "../../utils";
+
 
 
 const WeatherCard = props => {
-    const {cityName} = props;
+    const {weatherData} = props;
+    //const day = getDayName(new Date(weatherData.dt*1000).getDay());
+    const {list} = props.weatherData;
+    const currentWeatherDetail = list[0];
+    const day = getDayName(new Date(currentWeatherDetail.dt_txt).getDay());
+    const skyDescription = currentWeatherDetail.weather[0].main;
+    
+    let date = [];
+    const currentDateHours = new Date(currentWeatherDetail.dt_txt).getHours();
+    const result = list.filter(obj => {
+        const nextDate = new Date(obj.dt_txt).getDate();
+        const nextDateHours = new Date(obj.dt_txt).getHours();
+        if(!date.includes(nextDate) && currentDateHours == nextDateHours)
+        {
+            date.push(nextDate);
+            return true;
+        }
+        return false;
+      });
 
-    const fetchWeatherData = async () =>{
-        const resp = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${APP_ID}`);
-        console.log(await resp.json());
-    }
+    console.log(result);
+    return weatherData.cod == "200"  ? (
+        <div className="weatherCardContainer">
+            <div>
+            <h3 className="weatherCardHeaderText">{weatherData.city.name}</h3>
+            <h4 className="weatherCardHeaderText">{day}</h4>
+            <h5 className="weatherCardHeaderText">{skyDescription}</h5>
 
-    useEffect(() =>{
-        fetchWeatherData();
-    })
-
-    return (
-        <div>
-            <h2>Weather Card</h2>
-            <h5>{cityName}</h5>
+            </div>
+            <div class="currentWeatherContainer">
+                <div class="temperatureSection">
+                    <img className="avatar" alt="weather" src={`http://openweathermap.org/img/w/${currentWeatherDetail.weather[0].icon}.png`}/>
+                    <h3 className="custom-h3">38</h3>
+                    <h4 className="degrees-h4">°C</h4>
+                </div>
+                <div class="detailSection">
+                    <p className="temperatureDetails">Pressure: {currentWeatherDetail.main.pressure} hPa</p>
+                    <p className="temperatureDetails">Humidity: {currentWeatherDetail.main.humidity}%</p>
+                    <p className="temperatureDetails">Wind Speed: {currentWeatherDetail.wind.speed} m/s</p>
+                </div>
+            </div>
         </div>
-    );
+    ) : (<div>Result Not Found.</div>);
 };
 
 export default WeatherCard;
